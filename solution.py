5505c0417b37c7802825f6470c3792dd71a5c4d0
Mia-Ahrens <mahrens4@edisonohio.edu>
2026-06-15 18:34:08 -0400

    Create solution.py along with code.
    Included needed information such as name, age, birthday, hobby, calculation, etc

---
+++ solution.py
@@ -0,0 +1,26 @@
+# Topic 02 Collaborative Assignment
+# Your Name: Mia Ahrens
+# Date: 6/15/26
+
+# --- STARTER CODE ---
+# Step 1: Store some information about yourself
+name = "Mia Ahrens"           # Replace with your name
+favorite_number = 18  # Replace with your favorite number
+
+# Step 2: Write an expression using your variables
+# Example: double = favorite_number * 2
+birth_date_day = favorite_number
+
+# Step 3: Print a summary
+# Example: print("My name is", name)
+period = "."
+print("Hi! My name is", name ,"and my favorite number (which is also my birthday) is", favorite_number,period)
+
+# --- YOUR CODE BELOW THIS LINE ---
+birthday_year = 2012
+current_year = 2026
+favorite_hobby = "reading"
+
+age = current_year - birthday_year
+
+print("Additionally, I am ", age, " years old, and I love", favorite_hobby,".")
